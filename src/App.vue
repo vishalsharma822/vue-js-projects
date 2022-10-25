918ec965db31fd06bed870adc317a6c9d08d1f0b
vishalsharmacodeclouds <vishal.sharma@codeclouds.in>
2022-10-25 19:14:23 +0530

    for-loop-example

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <ForLoop />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ForLoop from './components/ForLoop.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    ForLoop
   }
 }
 </script>
---
+++ src/components/ForLoop.vue
@@ -0,0 +1,43 @@
+<template>
+<h1> Foor Loop in vue JS</h1>
+<table border="1">
+    <tr><td> Cars - Company</td></tr>
+    <tr v-for="company in cars" :key="company">
+        <td>{{ company }}</td>
+    </tr>
+</table>
+<br /> 
+
+<table border="1">
+    <tr><td> Users- Record (Array-of-Objects)</td></tr>
+    <tr v-for="student in users" :key="student">
+        <td>Name : {{ student.name }} <br>
+        Email : {{ student.email }}</td>
+    </tr>
+</table>
+</template>
+
+<script>
+export default {
+    name: 'ForLoop',
+    data() {
+        return {
+            cars: ['tata', 'maruti', 'volvo', 'audi', 'bmw'],
+            users:[
+                {
+                    name : 'vishal',
+                    email : 'vishal@gmail.com'
+                },
+                {
+                    name : 'rahul',
+                    email : 'rahul@gmail.com'
+                },
+                {
+                    name : 'ram',
+                    email : 'ram@gmail.com'
+                }
+            ]
+        }
+    }
+}
+</script>
