a6b09e34886094bd0f99389f830220e7f0f09b5c
vishalsharmacodeclouds <vishal.sharma@codeclouds.in>
2022-10-26 19:19:55 +0530

    padd-data-from-parent-to-child

---
+++ src/components/ChildComp.vue
@@ -0,0 +1,44 @@
+<template>
+<H2> Child Component</H2>
+<h3>{{ user.name }}</h3>
+
+<ul>
+    <h5>Data coming from Parent - Array</h5>
+    <li v-for="student in students" :key="student">
+        {{ student.name }}
+    </li>
+</ul>
+
+<button v-on:click="getData(),showHide()"> Get Student From Parent</button>
+<div class="showHide">
+    <h5>Data coming from Parent - Array of object</h5>
+    <ol>
+        <li v-for="employee in getData().employee" :key="employee">
+            {{ employee.name }}
+        </li>
+    </ol>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ChildComp',
+
+    props: {
+        user: Object,
+        getData: Function,
+        students:Array
+    },
+    methods:{
+        showHide(){
+            document.querySelector('.showHide').style.display = "block";
+        }
+    }
+}
+</script>
+
+<style>
+    .showHide{
+        display: none;
+    }
+</style>
---
+++ src/components/ParentComp.vue
@@ -0,0 +1,38 @@
+<template>
+<h2> Hello Parent </h2>
+<ChildComp :user="users" :getData='getData' :students='students'/>
+</template>
+
+<script>
+import ChildComp from './ChildComp.vue'
+export default {
+    name: 'ParentComp',
+    components: {
+        ChildComp
+    },
+    data() {
+        return {
+            users: {
+                name: 'vishal',
+                email: 'vishal@gmail.com'
+            },
+            students:[
+                    {name:'student 1', email:'email1@gmail.com'},
+                    {name:'student 2', email:'email2@gmail.com'},
+                ]
+        }
+    },
+    methods: {
+        getData(){
+            
+            return {
+                msg : 'hai',
+                employee:[
+                    {name:'employee 1', email:'employee1@gmail.com'},
+                    {name:'employee 2', email:'employee2@gmail.com'},
+                ]
+            }
+        }
+    }
+}
+</script>
