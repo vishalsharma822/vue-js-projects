02ae106931c30a102fe674a5bdb6abf2c139c382
vishalsharmacodeclouds <vishal.sharma@codeclouds.in>
2022-10-31 19:33:03 +0530

    form-modifier

---
+++ src/components/FormModifier.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>Form-modifier</h2>
+    <input type="text" v-model.lazy="res">
+    <h2> Data :- {{ res }}</h2>
+</template>
+
+<script>
+    export default{
+        name: 'FomrModifier',
+        data(){
+            return {
+                res :''
+            }
+        }
+    }
+</script>
